76f2ee8692c659961db62e05ebd5c34dce87c6a7
AbdullahSholi <groupgroup060@gmail.com>
2025-04-14 10:08:38 +0300

    Apply unit testing for JsonDataFormatStrategy class
    CanProcessWeatherData_WhenEnterValidData_ShouldReturnTrue
    
    CantProcessWeatherData_WhenEnterInValidData_ShouldReturnFalse
    
    ProcessWeatherData_WhenEnterValidWeatherData_ShouldProcessWeatherData

---
+++ BuildRealtimeWeatherMonitoringAndReportingService.Tests/JsonDataFormatStrategyShould.cs
@@ -0,0 +1,47 @@
+﻿using BuildRealtimeWeatherMonitoringAndReportingService.Deserialization;
+using BuildRealtimeWeatherMonitoringAndReportingService.Entities;
+using BuildRealtimeWeatherMonitoringAndReportingService.WeatherDataFormatsStrategyPattern;
+using Moq;
+
+namespace BuildRealtimeWeatherMonitoringAndReportingService.Tests;
+
+public class JsonDataFormatStrategyShould
+{
+    [Theory]
+    [InlineData("{}")]
+    public void CanProcessWeatherData_WhenEnterValidData_ShouldReturnTrue(string weatherData)
+    {
+        // Arrange
+        var canProcessWeatherData = weatherData.Trim().StartsWith("{") && weatherData.Trim().EndsWith("}");
+        
+        // Act & Assert
+        if(canProcessWeatherData)
+            Assert.True(weatherData.Trim().StartsWith("{") && weatherData.Trim().EndsWith("}"));
+    }
+
+    [Theory]
+    [InlineData("{R")]
+    public void CantProcessWeatherData_WhenEnterInValidData_ShouldReturnFalse(string weatherData)
+    {
+        // Arrange
+        var canProcessWeatherData = weatherData.Trim().StartsWith("{") && weatherData.Trim().EndsWith("}");
+        
+        // Act & Assert
+        if(!canProcessWeatherData)
+            Assert.False(canProcessWeatherData); 
+    }
+
+    [Theory]
+    [InlineData("{\"Location\": \"City Name\", \"Temperature\": 32, \"Humidity\": 40}")]
+    public void ProcessWeatherData_WhenEnterValidWeatherData_ShouldProcessWeatherData(string weatherData)
+    {
+        var sut = new JsonDataFormatStrategy();
+        
+        var result = sut.ProcessWeatherData(weatherData);
+
+        Assert.NotNull(result);
+        Assert.Equal(32, result.Temperature);
+        Assert.Equal(40, result.Humidity);
+    }
+    
+}
