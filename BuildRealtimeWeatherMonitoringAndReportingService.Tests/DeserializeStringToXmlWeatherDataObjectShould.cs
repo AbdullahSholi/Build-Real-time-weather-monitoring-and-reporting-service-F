117c39fe85059d49bad97a1edea9bfeb201f5e2c
AbdullahSholi <groupgroup060@gmail.com>
2025-04-14 12:44:00 +0300

    Apply unit testing for Deserialization classes
    DeserializeJsonStringToJsonWeatherDataObject_WhenEnterValidJsonString_ThenReturnCorrectWeatherDataObject
    
    DeserializeJsonStringToJsonWeatherDataObject_WhenEnterInValidJsonString_ThenThrowsException
    
    DeserializeXmlStringToXmlWeatherDataObject_WhenEnterValidXmlString_ThenReturnCorrectWeatherDataObject
    
    DeserializeXmlStringToXmlWeatherDataObject_WhenEnterInValidXmlString_ThenThrowsException

---
+++ BuildRealtimeWeatherMonitoringAndReportingService.Tests/DeserializeStringToXmlWeatherDataObjectShould.cs
@@ -0,0 +1,34 @@
+﻿using System.Xml.Serialization;
+using BuildRealtimeWeatherMonitoringAndReportingService.Deserialization;
+using BuildRealtimeWeatherMonitoringAndReportingService.Entities;
+using Moq;
+
+namespace BuildRealtimeWeatherMonitoringAndReportingService.Tests;
+
+public class DeserializeStringToXmlWeatherDataObjectShould
+{
+    [Theory]
+    [InlineData("<WeatherData><Location>City Name</Location><Temperature>32</Temperature><Humidity>40</Humidity></WeatherData>")]
+    public void DeserializeXmlStringToXmlWeatherDataObject_WhenEnterValidXmlString_ThenReturnCorrectWeatherDataObject(string toDeserialize)
+    {
+        // Assert
+        
+        // Act
+        var sut = DeserializeStringToXmlWeatherDataObject.DeserializeXmlStringToXmlWeatherDataObject(toDeserialize);
+        
+        // Assert
+        Assert.Equal("City Name", sut.Location);
+        Assert.Equal(32, sut.Temperature);
+        Assert.Equal(40, sut.Humidity);
+    }
+    
+    [Theory]
+    [InlineData("{\"Location\": \"City Name\", \"Temperature\": 32, \"Humidity\": 40}")]
+    public void DeserializeXmlStringToXmlWeatherDataObject_WhenEnterInValidXmlString_ThenThrowsException(string toDeserialize)
+    {
+        // Assert
+        
+        // Act && Assert
+        Assert.Throws<InvalidOperationException>(() => DeserializeStringToXmlWeatherDataObject.DeserializeXmlStringToXmlWeatherDataObject(toDeserialize));
+    }
+}
